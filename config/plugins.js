fd21f00a8c69651c96b66cbfeb0e12fe5abc59cc
PilyaP <ppaexpert@gmail.com>
2023-12-04 21:23:36 +0200

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -10,7 +10,7 @@ module.exports = {
         termsOfService: false,
         contact: {
           name: "Pasha Team",
-          email: "pavlo@gmail.com",
+          email: "ppaexpert@gmail.com",
           url: "https://nevada-frontend.vercel.app/",
         },
         license: {
