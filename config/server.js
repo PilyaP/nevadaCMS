9e37e16783a51eb3d0e34ae6d4aaa532ebdadac9
PilyaP <ppaexpert@gmail.com>
2023-12-04 23:26:01 +0200

    mail

--- config/server.js
+++ config/server.js
@@ -1,10 +1,25 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  email: {
+    provider: "sendmail", //
+    providerOptions: {
+      host: env("EMAIL_SMTP_HOST"), // SMTP Host
+      port: env.int("EMAIL_SMTP_PORT"), // SMTP Port
+      auth: {
+        user: env("EMAIL_SMTP_USER"),
+        pass: env("EMAIL_SMTP_PASS"),
+      },
+    },
+    settings: {
+      defaultFrom: "info@ppcwarehouses.com",
+      defaultReplyTo: "info@ppcwarehouses.com",
+    },
   },
 });
