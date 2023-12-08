6cd3574af24f1a280ef9162d9680af493273a995
PilyaP <ppaexpert@gmail.com>
2023-12-08 23:29:36 +0200

    ok

--- config/plugins.js
+++ config/plugins.js
@@ -54,15 +54,15 @@ module.exports = ({ env }) => ({
       },
     },
   },
-// "users-permissions":
-// {
-//   config:{
-//     jwt:{expiresIn: '28d'},
-//     register:{
-//       allowedFields: ['nickname']
-//     },
+"users-permissions":
+{
+  config:{
+    jwt:{expiresIn: '28d'},
+    register:{
+      allowedFields: ['nickname']
+    },
 
-//   }
-// }
+  }
+}
   // ... другие настройки ...
 });
